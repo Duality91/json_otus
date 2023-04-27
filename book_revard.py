91365405fa72546fdfd0e8013574d36f4881e894
Margarita <iloverock666@mail.ru>
2023-04-27 14:10:30 +0300

    redacted books distribution

--- book_revard.py
+++ book_revard.py
@@ -12,6 +12,7 @@ with open(JSON_FILE_PATH) as f:
         users_dict["gender"] = f"{q['gender']}"
         users_dict["address"] = f"{q['address']}"
         users_dict["age"] = q['age']
+        users_dict['books'] = []
         lst_users.append(users_dict)
 
 
@@ -19,40 +20,33 @@ with open(CSV_FILE_PATH) as t:
     rd = csv.DictReader(t)
     for row in rd:
         book_dict = {}
-        book_dict["Title"] = f"{row['Title']}"
-        book_dict['Author'] = f"{row['Author']}"
-        book_dict['Pages'] = f"{row['Pages']}"
-        book_dict['Genre'] = f"{row['Genre']}"
+        book_dict["title"] = f"{row['Title']}"
+        book_dict['author'] = f"{row['Author']}"
+        book_dict['pages'] = int(f"{row['Pages']}")
+        book_dict['genre'] = f"{row['Genre']}"
         lst_books.append(book_dict)
 
+# calculate how many books to distribute to each user
+num_users = len(lst_users)
+num_books = len(lst_books)
+books_per_user = num_books // num_users
+extra_books = num_books % num_users
 
-full_round = len(lst_books) // len(lst_users) #число равных кругов распределения
-group_books = []
-i = 0 #счетчик на формирования пакета книг исходя из формулы количества целовичленного деления кол0ва книг на колво юзеров
-p = 0 #переключение раздачи на следующего юзера
-n = 0 #всего раздали книг
-for t in lst_books: #пока книг меньше 3(целое значение книг на каждого) создаем список книг для юзера
-    group_books.append(t)
-    i += 1
-    n += 1
-    if i == full_round and (full_round * len(lst_users) >= n):
-        lst_users[p]["books"] = group_books
-        i = 0
-        p += 1
-        group_books = []
-
-p = 0 #распределение остатка книг
-if len(lst_books) % len(lst_users) != 0:
-    while len(lst_books) >= n:
-        lst_users[p]["books"].append(lst_books[n - 1])
-        p += 1
-        n += 1
-print(lst_users)
+# distribute books to each user
+book_idx = 0
+for user in lst_users:
+    # distribute the "equal" number of books
+    for i in range(books_per_user):
+        user['books'].append(lst_books[book_idx])
+        book_idx += 1
+    # distribute any remaining books
+    if extra_books > 0:
+        user['books'].append(lst_books[book_idx])
+        book_idx += 1
+        extra_books -= 1
+        print(book_idx, books_per_user)
 
 
-with open("files/result.json", "w")as f:  #сохранение результата в файл
+with open("result.json", "w")as f:  #сохранение результата в файл
     s = json.dumps(lst_users, indent=4)
     f.write(s)
-
-
-
